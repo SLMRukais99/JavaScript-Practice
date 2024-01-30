0a2591bcf54180e7b8763e027d87310cc9965499
SLMRukais99 <mohamedrukais@gmail.com>
2024-01-30 10:30:08 +0530

    01-01-2024

---
+++ 01-01-2024/script.js
@@ -0,0 +1,49 @@
+//node - it is an engine used to run the server
+//MERN
+//1.MangoDB - No sql - without object
+//2.Express JS - used to get the data from server
+//3.React JS
+//4.Node JS
+
+//In web technology, the frame work is responsible for security and folder structure
+
+//React JS - Facebook, Instagram
+
+//MEAN
+// 1. MangoDB
+// 2. Express JS
+// 3. Angular JS
+// 4. Node JS
+
+//MEVN
+// 1. MangoDB
+// 2. Express JS
+// 3. Vue JS
+// 4. Node JS
+
+//PERN
+//1.PostgreSQL
+//2.Express JS - used to get the data from server
+//3.React JS
+//4.Node JS
+
+//LAMP
+// 1.Linux
+// 2.Appache
+// 3.MySql
+// 4.PHP
+
+//Three type of function in Java Script
+// 1. default function
+// 2. Parameterized Function
+// 3. return Function
+
+//Here each part called as components in react.
+
+//2 types of components 
+// 1. Functional Components
+// 2 class components
+
+// databases
+//sql,olm
+//NoSql
