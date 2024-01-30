0b2ce771ba91ad1847d1dc8b3e9b16eb929fe543
SLMRukais99 <mohamedrukais@gmail.com>
2024-01-30 10:36:14 +0530

    02-01-2024

---
+++ 02-01-2024/js/script.js
@@ -0,0 +1,138 @@
+//standard of the JavaScript is EcmaScript6
+
+//typescript - Microsoft
+//typescript which is used to solve the problem in JavaScript. but browser can't understand the typeScript. 
+//when the typeScript compile,  It will give the Java Script, so then browser can easily understand.
+
+//var,let,const
+
+//var
+
+/*
+var x;
+console.log(x);
+
+ var x = 25;
+console.log(x);
+*/
+
+/* ES6
+let x = 35;
+console.log(x);
+*/
+
+/*
+const x = 10;
+console.log(x);
+*/
+
+//Data Types
+
+/*
+primitive data type
+1. Number
+2. String
+3. Boolean
+4. Null
+5. Symbol, Bigit 
+*/
+
+/*
+Preferance data type
+1. array
+2. object
+3. Function
+*/
+
+/*
+var x = 10;
+console.log(typeof x);
+*/
+
+/*
+var x = "10";
+console.log(typeof x);
+*/
+
+/*
+var x = "Rukais";
+let bool = true;
+
+console.log(x,typeof x, bool, typeof bool);
+*/
+
+//Type Casting
+/*
+// 1. number to String
+// 2. String to Number
+// 3. boolean to nUmber
+*/
+
+/*
+Type - 01
+var x = "50";
+x = Number(x);
+console.log(x, typeof x);
+*/
+
+/*
+Type - 02
+var x = "50";
+x = parseInt(x);
+console.log(x, typeof x);
+*/
+
+/*
+var y = "50.6";
+y = parseInt(y);
+console.log(y);
+*/
+
+
+/*
+var x = 50.6;
+x = x.toString();
+console.log(x, typeof x); 
+*/
+/*
+var x = 50.6;
+x = Boolean(x);
+console.log(x, typeof x);
+
+if(x){
+    console.log("This is not zero");
+}
+else{
+    console.log("This is Zero");
+}
+*/
+
+//
+/*
+1.Array
+2.Function
+3.Object
+*/
+
+//Array
+// let y = [10,20,30,40];
+// console.log(y);
+// console.log(y, typeof y);
+
+//object
+// let a = {
+//     name : "Rukais",
+//     age : 15
+// }
+// console.log(a, typeof a);
+
+//function
+// let abc = function(){
+//     console.log("This is function");
+// }
+// console.log(abc, typeof abc);
+
+/*
+let b = null;
+console.log(b, typeof b);
+*/
