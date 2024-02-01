9eee094e7b882217c7506645185cbb2540daa80c
SLMRukais99 <mohamedrukais@gmail.com>
2024-02-01 19:22:18 +0530

    03-01-2024

---
+++ 03-01-2024/script.js
@@ -0,0 +1,65 @@
+/*
+let fname = "Seelan";
+console.log("First Name",fname);
+
+let lname = fname;
+console.log("Last Name",lname);
+*/
+/*
+let fname = {
+    name : 'seelan'
+};
+console.log('First Name', fname.name); //First name seelan
+
+let lname = fname;
+console.log('Last Name',lname.name); //Last Name seelan
+
+lname.name = "Yoha";
+console.log('Last Name',lname.name);
+console.log('First Name', fname.name);
+
+fname.name = "vathan";
+console.log('Last Name',lname.name);
+console.log('First Name', fname.name);
+*/
+
+//operator
+/*
+let a,b,c;
+a = 10;
+b = 5;
+c = 3;
+console.log(a,b,c);
+const d=41, e=15, f=16;
+console.log(d,e,f);
+*/
+
+let x = 10;
+let y = 3;
+console.log(x+y); //13
+console.log(x-y); //7
+console.log(x*y); //30
+console.log(x/y); //3.333
+console.log(x%y); //1
+
+let lname = "seelan";
+let fname = "yoha";
+console.log(fname+" "+lname);
+
+let a=2;
+a = a**3
+console.log(a);
+
+let c = 5;
+let d = 3;
+console.log(c==d);
+console.log(c!=d);
+console.log(c>=d);
+console.log(c<=d);
+
+let l = '50';
+let o = 50;
+console.log(l===o); //false
+console.log(l==o); //true
+console.log(l!=o); //false
+console.log(l!==o); //true
